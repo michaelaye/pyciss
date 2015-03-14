0c04c6d40d07386b301f823a3e5579f5b45addad
Michael Aye <michael.aye@lasp.colorado.edu>
2015-03-14 17:18:51 -0600

    adding some image exploration tools

--- pyciss/plotting.py
+++ pyciss/plotting.py
@@ -1,2 +1,57 @@
+import matplotlib.pyplot as plt
+# from skimage.exposure import equalize_hist
+from IPython.html.widgets import interact, fixed
+import numpy as np
+import gdal
+
+
+interpolators = ['none', 'nearest', 'bilinear', 'bicubic',
+                 'spline16', 'spline36', 'hanning', 'hamming',
+                 'hermite', 'kaiser', 'quadric', 'catrom', 'gaussian',
+                 'bessel', 'mitchell', 'sinc', 'lanczos']
+
+
 def lookup_rcparam(rcParams, pattern):
     return [i for i in rcParams.keys() if pattern in i]
+
+
+def myimshow(img, vmin, vmax, i, cmap='gray'):
+    fig, ax = plt.subplots(nrows=2, figsize=(10, 10))
+    ax, ax2 = ax
+    ax.imshow(img, vmin=vmin, vmax=vmax, aspect='auto',
+              interpolation=interpolators[i],
+              cmap=cmap)
+    ax.set_title('vmin: {:.2f}, vmax: {:.2f}, interpolator:{}'
+                 .format(vmin, vmax, interpolators[i]))
+    tohist = img[~np.isnan(img)]
+    p1, p99 = np.percentile(tohist, (0.5, 99.5))
+    ax2.hist(img[~np.isnan(img)], 100, range=(p1, p99))
+    plt.show()
+
+
+def myinteract(img):
+    min_ = round(np.nanmin(img), 4)
+    max_ = round(np.nanmax(img), 4)
+    p30, p70 = np.percentile(img[~np.isnan(img)], (30, 70))
+    delta = round((p30-min_)/50, 5)
+    interact(myimshow, img=fixed(img), vmin=(min_, p30, delta),
+             vmax=(p70, max_, delta), i=(0, len(interpolators)-1))
+
+
+def gdal_explorer(fnames):
+    def imshow(img_number, vmin, vmax, cmap='gray'):
+        fig, ax = plt.subplots(nrows=2, figsize=(10, 10))
+        ax, ax2 = ax
+        ds = gdal.Open(fnames[img_number])
+        data = ds.ReadAsArray()
+        try:
+            data[data < -2e+38] = np.NaN
+        except ValueError:
+            pass
+
+
+def imshowlowhigh(data, low=10, high=90):
+    fig, ax = plt.subplots()
+    plow, phigh = np.percentile(data[~np.isnan(data)], (low, high))
+    ax.imshow(data, vmin=plow, vmax=phigh, cmap='gray')
+    return fig
