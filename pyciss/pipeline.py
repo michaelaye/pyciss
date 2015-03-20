423d040c91fa8ed264fa3fdb037139b4d52411c4
K.-Michael Aye <kmichael.aye@gmail.com>
2015-03-19 20:30:35 -0600

    adding remapping and calibrate_many. Latter not working due to IsisPool bug, apparently.

--- pyciss/pipeline.py
+++ pyciss/pipeline.py
@@ -1,13 +1,15 @@
-from pysis.isis import ciss2isis, cisscal, spiceinit, ringscam2map
+from __future__ import division, print_function
+from pysis.isis import ciss2isis, cisscal, spiceinit, ringscam2map, getkey,\
+    editlab
 from pysis.util import file_variations
+from pysis import IsisPool
 import gdal
 import numpy as np
 from os.path import join as pjoin
 import os
 from pyciss import plotting
 from pyciss.io import dataroot
-
-
+from . import io
 ISISDATA = os.environ['ISIS3DATA']
 
 
@@ -18,6 +20,18 @@ def calibrate_ciss(img_name, name_only=False):
     if name_only:
         return map_name
     ciss2isis(from_=img_name, to=cub_name)
+    targetname = getkey.check_output(from_=cub_name,
+                                     grp='instrument',
+                                     keyword='targetname')
+    # forcing the target name to Saturn here, because some observations of
+    # the rings have moons as a target, but then the standard map projection
+    # onto the Saturn ring plane fails.
+    # see also
+    # https://isis.astrogeology.usgs.gov/IsisSupport/index.php/topic,3922.0.html
+    if targetname.lower() != 'saturn':
+        editlab(from_=cub_name, options='modkey',
+                keyword='TargetName', value='Saturn',
+                grpname='Instrument')
     spiceinit(from_=cub_name, cksmithed='yes', spksmithed='yes',
               shape='ringplane')
     cisscal(from_=cub_name, to=cal_name)
@@ -25,6 +39,26 @@ def calibrate_ciss(img_name, name_only=False):
                  map=pjoin(ISISDATA,
                            'base/templates/maps/ringcylindrical.map'))
     return map_name
+
+
+def remapping(img_name):
+    (cal_name, map_name) = file_variations(img_name,
+                                           ['.cal.cub', '.map.cal.cub'])
+    print("Mapping", cal_name, "to", map_name)
+    mapfname = pjoin(io.HOME, 'data', 'ciss', 'opus', 'ringcylindrical.map')
+    ringscam2map(from_=cal_name, to=map_name, map=mapfname, pixres='map')
+
+
+def calibrate_many(images):
+    images = [[img_name, ] + file_variations(img_name, ['.cub', '.cal.cub',
+                                                        '.map.cal.cub'])
+              for img_name in images]
+
+    with IsisPool() as isis_pool:
+        for img_name, cub_name, cal_name, map_name in images:
+            isis_pool.ciss2isis(from_=img_name, to=cub_name)
+
+    return images
 
 
 def process_image(fname):
@@ -44,4 +78,3 @@ def pipeline(fname):
         process_image(map_name)
     except AttributeError:
         return "Problem with {}".format(fname)
-
