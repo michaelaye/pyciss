6018e95106127c25f4c0feecde14c6350143a11f
K.-Michael Aye <kmichael.aye@gmail.com>
2016-05-26 11:38:23 -0600

    Bump version: 0.5.0 → 0.5.1

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name=DISTNAME,
-    version="0.5.0",
+    version="0.5.1",
     packages=find_packages(),
 
     install_requires=['pandas'],
