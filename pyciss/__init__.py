5ae1cd74f4e1f287a4988d69fd2c6795c2fa3f7a
K.-Michael Aye <kmichael.aye@gmail.com>
2016-12-29 23:46:52 -0700

    add NullHandler to avoid unconfigured messages on sys.stderr

--- pyciss/__init__.py
+++ pyciss/__init__.py
@@ -1,1 +1,6 @@
 __version__ = '0.6.4'
+
+import logging
+
+# this prevents messages sent to sys.stderr if no logging was configured on application-side
+logging.getLogger('pyciss').addHandler(logging.NullHandler())
