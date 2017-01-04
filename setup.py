5ba17480c8a10d6347a728829015e5e61ff8c564
K.-Michael Aye <kmichael.aye@gmail.com>
2017-01-04 00:29:33 -0700

    Bump version: 0.7.1 → 0.7.2

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name=DISTNAME,
-    version="0.7.1",
+    version="0.7.2",
     packages=find_packages(),
 
     install_requires=['pandas', 'numpy', 'matplotlib', 'pysis'],
