83eca9c894bf21c7f0a4dcf502ebe49e232438ec
K.-Michael Aye <kmichael.aye@gmail.com>
2017-02-07 16:59:23 -0700

    Bump version: 0.7.7 → 0.8.0

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name=DISTNAME,
-    version="0.7.7",
+    version="0.8.0",
     packages=find_packages(),
 
     install_requires=['pandas', 'numpy', 'matplotlib', 'pysis', 'astropy'],
