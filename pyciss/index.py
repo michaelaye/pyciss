1b880927b10b0306932766508b59c8aa49a244e2
K.-Michael Aye <kmichael.aye@gmail.com>
2017-02-07 16:43:51 -0700

    add ring images filter and config reading

--- pyciss/index.py
+++ pyciss/index.py
@@ -1,25 +1,46 @@
-from .io import config
-import pandas as pd
 from pathlib import Path
+
+import numpy as np
+import pandas as pd
+
+from .io import config
 
 
 def read_cumulative_iss_index():
     "Read in the whole cumulative index and return dataframe."
-    if indexdir is None:
-        try:
-            indexdir = Path(config['pyciss_indexdir'])
-        except KeyError:
-            print("Did not find the key `pyciss_indexdir` in the config file.")
-            return
+    try:
+        indexdir = Path(config['pyciss_index']['path'])
+    except KeyError:
+        print("Did not find the key `[pyciss_index][path]` in the config file.")
+        return
+    path = indexdir / 'cumindex.tab.hdf'
+    df = pd.read_hdf(path, 'df')
+    # replace DPS Nan values (-1e32) with real NaNs
+    return df.replace(-1.000000e+32, np.nan)
 
-    savepath = indexdir / 'cumindex.tab.hdf'
-    if savepath.exists():
-        return pd.read_hdf(savepath, 'df')
-    else:
-        df = index_to_df(indexdir / 'cumindex.tab')
-        df.to_hdf(savepath, 'df')
-        return df
 
+def read_ring_images_index():
+    """Filter cumulative index for ring images.
+
+    This is done by matching the column TARGET_DESC to contain the string 'ring'
+
+    Returns
+    -------
+    pandas.DataFrame
+        data table containing only meta-data for ring images
+    """
+    meta = read_cumulative_iss_index()
+    ringfilter = meta.TARGET_DESC.str.contains('ring', case=False)
+    return meta[ringfilter]
+
+
+#
+# def
+#     else:
+#         df = index_to_df(indexdir / 'cumindex.tab')
+#         df.to_hdf(savepath, 'df')
+#         return df
+#
 
 class IndexDB(object):
     def __init__(self, indexdir=None):
