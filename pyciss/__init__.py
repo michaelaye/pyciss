4a331e8e0e5ec77db38878e6762ef64a16f25d61
K.-Michael Aye <kmichael.aye@gmail.com>
2019-01-02 16:11:28 -0700

    add RingCube to main namespace

--- pyciss/__init__.py
+++ pyciss/__init__.py
@@ -1,6 +1,8 @@
-__version__ = '0.11.0'
+__version__ = "0.11.0"
 
 import logging
 
 # this prevents messages sent to sys.stderr if no logging was configured on application-side
-logging.getLogger('pyciss').addHandler(logging.NullHandler())
+logging.getLogger("pyciss").addHandler(logging.NullHandler())
+
+from .ringcube import RingCube
