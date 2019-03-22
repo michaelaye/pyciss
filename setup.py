185b9e2fa2b260e88c9178e18e21fe65c7adf882
K.-Michael Aye <kmichael.aye@gmail.com>
2019-03-22 18:30:10 -0500

    Bump version: 0.12.0 -> 0.12.1

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name=DISTNAME,
-    version="0.12.0",
+    version="0.12.1",
     packages=find_packages(),
 
     install_requires=['pandas', 'numpy', 'matplotlib', 'pysis', 'astropy', 'xarray', 'holoviews'],
